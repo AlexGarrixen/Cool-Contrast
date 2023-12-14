11ff53c0775414de5841742fa32390cdefd8b48e
alexTexis <alextexis.js@gmail.com>
2023-12-14 14:23:14 -0600

    feat: add rounded borders to panda-css config

---
+++ theme/tokens/border-radius.ts
@@ -0,0 +1,10 @@
+import { defineTokens } from "@pandacss/dev";
+
+export default defineTokens.radii({
+  xs: { value: "0.125rem" },
+  sm: { value: "0.25rem" },
+  md: { value: "0.375rem" },
+  lg: { value: "0.5rem" },
+  xl: { value: "0.75rem" },
+  "2xl": { value: "1rem" },
+});
--- theme/tokens/index.ts
+++ theme/tokens/index.ts
@@ -3,9 +3,11 @@ import { defineTokens } from "@pandacss/dev";
 import colors from "./colors";
 import spacing from "./spacing";
 import shadows from "./shadows";
+import radii from "./border-radius";
 
 export default defineTokens({
   colors,
   spacing,
   shadows,
+  radii,
 });
