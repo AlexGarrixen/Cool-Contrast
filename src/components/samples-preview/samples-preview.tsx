4d932e514f719ed7e23899864b594fda736a6286
alexTexis <alextexis.js@gmail.com>
2023-12-29 03:10:05 -0600

    style(samples-preview): remove heading

--- src/components/samples-preview/samples-preview.tsx
+++ src/components/samples-preview/samples-preview.tsx
@@ -3,18 +3,13 @@ import { css } from "@root/styled-system/css";
 import { SamplesGrid } from "./samples-grid";
 
 const classes = {
-  root: css({ my: "5" }),
-  heading: css({ color: "text-secondary", fontWeight: "500", mb: "2" }),
-  container: css({ bgColor: "bg-primary", p: "5", rounded: "xl", shadow: "xs" }),
+  root: css({ my: "5", bgColor: "bg-primary", p: "5", rounded: "xl", shadow: "xs" }),
 };
 
 export function SamplesPreview() {
   return (
     <section className={classes.root}>
-      <p className={classes.heading}>SAMPLES PREVIEW</p>
-      <div className={classes.container}>
-        <SamplesGrid />
-      </div>
+      <SamplesGrid />
     </section>
   );
 }
