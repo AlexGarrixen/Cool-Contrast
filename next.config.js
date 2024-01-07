4b2ea4cdfcc4a917f622f6bf6726342b22eeee20
alexTexis <alextexis.js@gmail.com>
2024-01-07 11:21:23 -0600

    chore(next.config): change domains deprecated option to remotePatterns

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
   },
 };
 
