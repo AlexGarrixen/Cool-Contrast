555281f0d48302c51d298952c2e8de9ad2ba2d81
alexTexis <alextexis.js@gmail.com>
2024-01-09 03:55:44 -0600

    feat(contrast suggestions): improve color scale generator

--- src/lib/contrast-suggestions/contrast-suggestions.ts
+++ src/lib/contrast-suggestions/contrast-suggestions.ts
@@ -1,5 +1,5 @@
 import Color from "color";
-import chroma from "chroma-js";
+import chroma, { type Scale } from "chroma-js";
 import _uniqWith from "lodash.uniqwith";
 
 import { contrastGuidelines } from "@/lib/wcag-text-contrast";
@@ -11,13 +11,48 @@ interface SuggestionResult {
   id: string;
 }
 
-function createColorScale(baseColor: string) {
-  const result: string[] = [];
-  const scale = chroma
-    .scale([chroma(baseColor).brighten(2), baseColor, chroma(baseColor).darken(2)])
-    .domain([0, 500]);
+interface ColorScaleOpts {
+  range?: [number, number];
+  stepsDistance?: number;
+  startInRange?: number;
+  stopInRange?: number;
+  direction?: "center" | "start";
+}
 
-  for (let i = 50; i < 500; i += 100) {
+function isDark(color: string) {
+  const luminance = chroma(color).luminance();
+  const umbral = 0.2;
+
+  return luminance < umbral;
+}
+
+function createColorScale(sourceColor: string, opts?: ColorScaleOpts) {
+  const defaultOpts = { range: [0, 500], stepsDistance: 100 } satisfies ColorScaleOpts;
+  const { range, startInRange, stopInRange, stepsDistance, direction } = Object.assign(
+    defaultOpts,
+    opts,
+  );
+  const [minRange, maxRange] = range;
+  const numSteps = Math.ceil(maxRange / stepsDistance);
+  let scale: Scale;
+
+  if (direction === "center") {
+    const steps = Math.floor(numSteps / 2);
+
+    scale = chroma
+      .scale([chroma(sourceColor).brighten(steps), sourceColor, chroma(sourceColor).darken(steps)])
+      .domain(range);
+  } else {
+    scale = !isDark(sourceColor)
+      ? chroma.scale([sourceColor, chroma(sourceColor).darken(numSteps)]).domain(range)
+      : chroma.scale([sourceColor, chroma(sourceColor).brighten(numSteps)]).domain(range);
+  }
+
+  const startRange = startInRange ?? minRange;
+  const stopRange = stopInRange ?? maxRange;
+  const result: string[] = [];
+
+  for (let i = startRange; i <= stopRange; i += stepsDistance) {
     const color = scale(i).hex();
 
     result.push(color);
@@ -50,26 +85,29 @@ function getBestScalesContrast(scaleA: string[], scaleB: string[]) {
 }
 
 export function createContrastSuggestions(bg: string, fg: string) {
-  const scaleBg = createColorScale(bg);
-  const scaleFg = createColorScale(fg);
-  let colors: SuggestionResult[] = [];
+  const scaleBg = createColorScale(bg, { direction: "center" });
+  const scaleFg = createColorScale(fg, { direction: "center" });
+  let result: SuggestionResult[] = [];
 
-  colors = [...getBestScalesContrast(scaleBg, scaleFg)];
+  result = [...getBestScalesContrast(scaleBg, scaleFg)];
 
-  if (colors.length === 0) {
-    const fallbackScale = ["#000000", "#ffffff"];
+  if (result.length <= 3) {
+    const fallbackScale = createColorScale("#000", {
+      range: [0, 950],
+      startInRange: 50,
+    });
 
-    colors = [
-      ...colors,
-      ...getBestScalesContrast(scaleBg, fallbackScale),
-      ...getBestScalesContrast(scaleFg, fallbackScale),
+    result = [
+      ...result,
+      ...getBestScalesContrast([bg], fallbackScale),
+      ...getBestScalesContrast([fg], fallbackScale),
     ];
   }
 
-  colors = colors.sort((a, b) => parseFloat(a.contrast) - parseFloat(b.contrast));
+  result = result.sort((a, b) => parseFloat(a.contrast) - parseFloat(b.contrast));
 
   return _uniqWith(
-    colors,
+    result,
     (objA: SuggestionResult, objB: SuggestionResult) => objA.fg === objB.fg && objA.bg === objB.bg,
   );
 }
