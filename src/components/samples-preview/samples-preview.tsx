3e75a18d7e546df09f99dab329ff53c1bd954ffb
alexTexis <alextexis.js@gmail.com>
2024-01-15 12:32:50 -0600

    feat(samples preview): toggle preview modes

--- src/components/samples-preview/samples-preview.tsx
+++ src/components/samples-preview/samples-preview.tsx
@@ -1,22 +1,31 @@
 "use client";
 
+import { useState } from "react";
 import { useAtomValue } from "jotai";
-import Color from "color";
 import { css } from "@root/styled-system/css";
+import Color from "color";
 
 import { background, foreground } from "@/store";
 
+import * as ToggleButton from "./toggle-button";
 import { MinimalistPage } from "./minimalist-page";
+import { SamplesGrid } from "./samples-grid";
 import { fgVar, bgVar } from "./utils";
 
 const classes = {
   root: css({ my: "6" }),
-  text: css({ textStyle: "text-lg", textAlign: "center", mb: "3", color: "text-secondary" }),
+  toggle: css({ textAlign: "center", mb: "6" }),
+};
+
+const previewModes = {
+  minimalistPage: "minimalist-page",
+  uiElements: "ui-elements",
 };
 
 export function SamplesPreview() {
   const bg = useAtomValue(background);
   const fg = useAtomValue(foreground);
+  const [previewMode, setPreviewMode] = useState<string>(previewModes.minimalistPage);
   const bgRgb = Color(bg).rgb().round().array().join(" ");
   const fgRgb = Color(fg).rgb().round().array().join(" ");
 
@@ -25,8 +34,13 @@ export function SamplesPreview() {
       className={classes.root}
       style={{ [fgVar as string]: fgRgb, [bgVar as string]: bgRgb }}
     >
-      <p className={classes.text}>Preview</p>
-      <MinimalistPage />
+      <div className={classes.toggle}>
+        <ToggleButton.Root defaultValue={previewMode} onChange={setPreviewMode}>
+          <ToggleButton.Item value={previewModes.minimalistPage}>Minimalist Page</ToggleButton.Item>
+          <ToggleButton.Item value={previewModes.uiElements}>UI Elements</ToggleButton.Item>
+        </ToggleButton.Root>
+      </div>
+      {previewMode === previewModes.minimalistPage ? <MinimalistPage /> : <SamplesGrid />}
     </section>
   );
 }
