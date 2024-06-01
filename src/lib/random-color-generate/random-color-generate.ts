cfc010b105162ca2919cefacd7d4f063e493d74d
alexTexis <alextexis.js@gmail.com>
2024-06-01 16:23:24 -0600

    fix(random color generator): set maximum attempts to avoid possible loops

--- src/lib/random-color-generate/random-color-generate.ts
+++ src/lib/random-color-generate/random-color-generate.ts
@@ -1,14 +1,22 @@
 import chroma from "chroma-js";
 
+import { fallbackColorPairs } from "./fallback-color-pairs";
+
 function generateHexColor() {
   return chroma.random().hex();
 }
 
-function getContrastColor(hexColor: string, attempts = 0, maxAttempts = 30) {
+function getRandomColorPair() {
+  const idx = Math.floor(Math.random() * fallbackColorPairs.length);
+
+  return fallbackColorPairs[idx];
+}
+
+function getContrastColor(hexColor: string, attempts = 0, maxAttempts = 100) {
   if (attempts >= maxAttempts) {
     console.warn("Max attempts reached, returning last generated color.");
 
-    return generateHexColor();
+    return undefined;
   }
 
   const generatedHexColor = generateHexColor();
@@ -17,13 +25,20 @@ function getContrastColor(hexColor: string, attempts = 0, maxAttempts = 30) {
   if (contrastRatio >= 4.5) {
     return generatedHexColor;
   } else {
-    return getContrastColor(hexColor);
+    return getContrastColor(hexColor, attempts + 1, maxAttempts);
   }
 }
 
 export function randomColorGenerate() {
-  const baseColor = generateHexColor();
-  const baseColorContrast = getContrastColor(baseColor);
+  let baseColor = generateHexColor();
+  let baseColorContrast = getContrastColor(baseColor);
+
+  if (!baseColorContrast) {
+    const randomColorPair = getRandomColorPair();
+
+    baseColor = randomColorPair.background;
+    baseColorContrast = randomColorPair.foreground;
+  }
 
   return { baseColor, baseColorContrast };
 }
